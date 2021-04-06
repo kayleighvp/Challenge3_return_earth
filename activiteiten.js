c4a47bc883ac7817b1578b37411852214edf4724
kayleighvp <62561981+kayleighvp@users.noreply.github.com>
2021-04-06 19:17:44 +0200

    test-upload

---
+++ activiteiten.js
@@ -0,0 +1,81 @@
+mijnActiviteitenLijst = [{
+    'type': 'Feature',
+    'properties': {
+      'description': '<h3>Fury Sport Bar</h3><p>1353 W Inyokern Rd, Ridgecrest, CA 93555, US. Fury Sport Bar is a new bar in the area, but is already a favorite with many people due to their good service. It is a 5 minute drive from the hotel.</p>',
+      'icon': 'bar'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-117.697590, 35.651730]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<h3>DJ Pub</h3><p>135 Garnet Ave, Ridgecrest, CA 93555, US. DJ Pub is known for their good cocktails, especially the bloody mary is a favorite. It is a 4 minute drive from the hotel.</p>',
+      'icon': 'bar'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-117.668040, 35.621260]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<h3>Barefoot Bar</h3><p>500 Essex Cir, Ridgecrest, CA 93555, US. Barefoot Bar also has a swimming pool in addition to their bar. Highly recommended for when it is warm outside. It is a 7 minute drive from the hotel.</p>',
+      'icon': 'bar'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-117.640350, 35.652360]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<h3>Spizzerinctum Land & Cattle</h3><p>Randsburg, Californië 93528, US. Spizzerinctum Land & Cattle is a small art gallery in town. It is a 26 minute drive from the hotel and recommended by the locals.</p>',
+      'icon': 'art-gallery'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-117.732800, 35.422510]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<h3>Montara park</h3><p>Barstow, Californië 92311, US. Make a nice bikeride in the Montara park for just &#36;5 an hour. It is just a 5 minute drive from the hotel.</p>',
+      'icon': 'bicycle'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-116.994982, 34.880987]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<h3>Pit Stop Bar & Grill</h3><p>560 Victor St, Barstow, CA 92311, US. Pit Stop Bar & Grill serves according to locals the best beer and great burgers. It is a 6 minute drive from the hotel.</p>',
+      'icon': 'bar'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-116.990380, 34.884050]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<h3>The 86ers Lounge Bar</h3><p>1309 E Main St, Barstow, CA 92311, US. The 86ers Lounge Bar offers a variety of stand up comedians acts in addition to tasty drinks. It is a 8 minute drive from the hotel.</p>',
+      'icon': 'bar'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-117.007040, 34.896130]
+    }
+  }
+];
+
+
+//'icon': 'art-gallery', 'icon': 'bicycle', 'icon': 'bar', 'icon': 'theatre', 'icon': 'star', 'icon': 'music'
